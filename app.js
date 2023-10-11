56f51fbd65f04092b9298e82be761deebce2a700
monkeycoder-09 <monkeycoder-09@users.noreply.github.com>
2023-10-12 02:47:38 +0530

    Five Rounds Score

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let playerScore = 0
+let compScore = 0
+
+
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"]
     const randomNum = Math.floor(Math.random() * 3)
@@ -15,29 +19,48 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return 'You tied! You both picked Paper'
     }
-    else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'You tied! You both picked Paper'
-    }
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return 'You Won! Paper Covers Rock'
     }
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
         return 'You Lost! Paper Covers Rock'
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
         return 'You Lost! Scissors cuts Paper'
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return 'You Won! Scissors cuts Paper'
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return 'You Won! Rock crushes Scissors'
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
         return 'You Lost! Rock crushes Scissors'
     }
 }   
 
 const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for ( let i = 0; i < 5; i++) {
+        const computerSelection = computerPlay();
+        (playRound(playerSelection, computerSelection));
+    }
+
+if (playerScore > compScore) {
+    return 'You won the game!'
+} else if (compScore > playerScore) {
+    return 'You lost the game!'
+} else {
+    return 'You both tied!'
+    }
+}
+
+console.log(game())
