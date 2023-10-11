51372f2150ef5b3dd2955add7c45eabdb06df6cf
monkeycoder-09 <monkeycoder-09@users.noreply.github.com>
2023-10-12 02:29:39 +0530

    Getting Player selection and Computer Selection

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"]
+    const choices = ["rock", "paper", "scissors"]
     const randomNum = Math.floor(Math.random() * 3)
     const compChoice = (choices[randomNum])
     return compChoice
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You tied! You both picked Rock'
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You tied! You both picked Scissors'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked Paper'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked Paper'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Won! Paper Covers Rock'
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lost! Paper Covers Rock'
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lost! Scissors cuts Paper'
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Won! Scissors cuts Paper'
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Won! Rock crushes Scissors'
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lost! Rock crushes Scissors'
+    }
+}   
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
