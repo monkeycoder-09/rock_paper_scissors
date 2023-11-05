36bdb37b911166e8eca60d8400604cd7ce1fcb8c
monkeycoder-09 <monkeycoder-09@users.noreply.github.com>
2023-11-05 18:58:38 +0530

    Add Reset Option

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let playerScore = 0
 let compScore = 0
+let roundCount = 0
 
 const rockButton = document.querySelector('.rock')
 const paperButton = document.querySelector('.paper')
 const scissorsButton = document.querySelector('.scissors')
 const result = document.querySelector('.result')
 const score = document.querySelector('.score')
+const resetButton = document.querySelector('.reset')
 
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"]
@@ -15,69 +17,88 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'rock') {
-        
-        updateScore();
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        
-        updateScore();
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        
-        updateScore();
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        playerScore++
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
         updateScore()
-        
-    }
-    else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        compScore++
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
         updateScore()
-        
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        compScore++
+        return 'You win! ${playerSelection} beats ${computerSelection}.';
+    } else {
+        compScore++;
         updateScore()
-        
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
-    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        playerScore++
-        updateScore()
-        
+    
+}
+
+function resetGame() {
+    playerScore = 0;
+    compScore = 0;
+    roundCount = 0;
+    result.textContent = " ";
+    score.textContent = "Player: 0 - Computer: 0";
+    enableButtons();
+}
+
+
+function updateScore() {
+    score.textContent = `Player: ${playerScore} - Computer: ${compScore}`;
+    checkWinner();
+  }
+
+
+  function checkWinner() {
+    if (playerScore >= 5) {
+        result.textContent = "Congratulations! You won the game!";
+        disableButtons();
+    } else if (compScore >= 5) {
+        result.textContent = "Sorry, you lost the game. Try again!";
+        disableButtons();
     }
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        playerScore++
-        updateScore()
-        
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        compScore++
-        updateScore()
-        
-    }
-}   
+}
+
+
+
+function disableButtons() {
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+}
+
+function enableButtons() {
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+}
+
 
 rockButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const result = playRound('rock', computerSelection);
-    resultDiv.textContent = result;
+    result.textContent = score;
     updateScore();
+    roundCount++;
 });
 
 paperButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const result = playRound('paper', computerSelection);
-    resultDiv.textContent = result;
+    result.textContent = score;
     updateScore();
+    roundCount++;
 });
 
 scissorsButton.addEventListener('click', () => {
     const computerSelection = computerPlay();
     const result = playRound('scissors', computerSelection);
-    resultDiv.textContent = result;
+    result.textContent = score;
     updateScore();
+    roundCount++;
 });
   
   function removeSelectionClasses() {
@@ -87,18 +108,9 @@ scissorsButton.addEventListener('click', () => {
     scissorsButton.classList.remove('scissors-selected');
   }
 
-function updateScore() {
-    score.textContent = `Player: ${playerScore} - Computer: ${compScore}`;
-    checkWinner();
-  }
 
-function checkWinner() {
-    if (playerScore >= 5) {
-        result.textContent = "Woohooo! You Won.";
-    } else if (compScore >= 5) {
-        result.textContent = "Oh no! You Lost."
-    }
-}
+  resetButton.addEventListener('click', resetGame)
+ 
 
 
 
